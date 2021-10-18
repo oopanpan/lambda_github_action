92f2908d76cea52b141e96acae4b4589a5936316
Pan Li <oopanpan@gmail.com>
2021-10-17 23:17:53 -0700

    "type":"module"

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ exports.handler = async (event) => {
     const max = 10;
     const val = _.random(max);
     const response = {
-        statusCode: 200,
-        body: `The random value (max: ${max}) is: ${val}`,
-    }
+		statusCode: 200,
+		body: `The random value (max: ${max}) is: ${val}`,
+	};
     return response;
 }
